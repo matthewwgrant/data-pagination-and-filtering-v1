cc8d5e03215cd02bff139dd69850c4b57078340c
Matthew Grant <matthewgrant.webdev@gmail.com>
2021-09-15 20:28:48 -0400

    created first function
    First function to display proper amount of users is loading and working for each page number

--- js/script.js
+++ js/script.js
@@ -17,14 +17,49 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+	startIndex = (page * 9) - 9;
+	endIndex = (page * 9) - 1;
+	const ul = document.querySelector('.student-list');
+	ul.innerHTML = '';
+
+	for ( let i = 0; i < list.length; i++ ) {
+		if ( i >= startIndex && i <= endIndex ) {
+			const li = document.createElement('li');
+			li.className = 'student-item cf';
+			ul.appendChild(li);
+			const divStudent = document.createElement('div');
+			divStudent.className = 'student-details';
+			li.appendChild(divStudent);
+			const img = document.createElement('img');
+			img.className = 'avatar';
+			img.src = list[i].picture.medium;
+			divStudent.appendChild(img);
+			console.log(li)
+			const h3 = document.createElement('h3');
+			h3.textContent = `${list[i].name.first} ${list[i].name.last}`;
+			divStudent.appendChild(h3);
+			const divJoined = document.createElement('div');
+			divJoined.className = 'joined-details';
+			li.appendChild(divJoined);
+			const span = document.createElement('span');
+			span.className = 'date';
+			span.innerHTML = `Joined ${list[i].registered.date}`;
+			divJoined.appendChild(span);
+
+		}
+	}
+}
 
 
+ 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
+ 
 
 
 // Call functions
+showPage(data, 1);
